5cbef2a317379539a83f706e20da5449e0f689fd
sidhearts <rajsiddharth21@gmail.com>
2021-03-18 15:16:42 +0530

    brands and career

--- src/component/Home.js
+++ src/component/Home.js
@@ -72,7 +72,7 @@ function Home() {
         </Navbar>
       </div>
 
-      <div className="content" style={{ height: "300vh" }}>
+      <div className="content pb-5">
         <Parallax bgImage="/images/football.jpg" strength={500}>
           <div style={{ height: 700 }}>
             <div style={inlineStyle}>
@@ -137,10 +137,170 @@ function Home() {
               </Carousel.Item>
             </Carousel>
           </div>
+          <h1 id="brands" className="texts pt-5">
+            Brands
+          </h1>
+          <hr className="head-hr" width="200px" />
+          <div className="text-center texts">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <Container className="pt-4">
+            <Row>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-1.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-2.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-3.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-4.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-5.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-6.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-7.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-8.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-9.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-10.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-11.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-12.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-13.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-14.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-15.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+              <Col xs={6} md={3} className="p-2 p-md-3">
+                <img
+                  src="/images/logo-16.png"
+                  alt="brand"
+                  className="rounded img-fluid brand-logo"
+                />
+              </Col>
+            </Row>
+          </Container>
+          <h1 id="career" className="texts pt-5">
+            Career
+          </h1>
+          <hr className="head-hr" width="200px" />
+          <div className="text-center texts">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
         </div>
       </div>
 
       {/* Footer */}
+      <div className="footer">
+        <Container>
+          <Row>
+            <Col xs={6} className="texts text-left p-3">
+              <small>&copy; 2021 all rights reserved</small>
+            </Col>
+            <Col xs={5} className="texts p-3">
+              <span>Contact us:</span>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
